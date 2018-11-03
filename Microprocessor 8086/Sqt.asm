94161f8234dc272bba392b260b97596220f43957
ABHILASH <35189926+abhilashiswithu@users.noreply.github.com>
2018-11-03 15:29:51 +0530

    Add files via upload

---
+++ Microprocessor 8086/Sqt.asm
@@ -0,0 +1,22 @@
+ASSUME CS:CODE, DS:DATA
+DATA SEGMENT
+        NUM1 DB 10H
+        COUNT DB 00H
+        ODD DB 01H
+DATA ENDS
+CODE SEGMENT
+        MOV AX,DATA
+        MOV DS,AX
+        MOV AX,NUM1
+        MOV CX,COUNT
+        MOV BX,ODD
+ AGAIN: SUB AX,BX
+        ADD BX,02H
+        INC CX
+        CMP AX,00H
+        JNZ AGAIN
+        MOV DX,CX
+        MOV AH,4CH
+        INT 21H
+ CODE ENDS
+ END START
