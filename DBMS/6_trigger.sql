26e90860ce5fa16bc8e0576cbf99e6e96e2b2869
ABHILASH <35189926+abhilashiswithu@users.noreply.github.com>
2018-11-24 15:49:03 +0530

    Create 6_trigger.sql

---
+++ DBMS/6_trigger.sql
@@ -0,0 +1,7 @@
+/*
+TRIGGERS
+department(dept_id,dept_name)
+employee(emp_id,name,dob,doj,sal,dept_id)
+emp_backup(emp_id,name,dob,doj,sal,dept_id,date_of_op,type_of_op)
+income_tax(emp_id,name,dob,doj,sal,dept_id,tax_amount)
+*/
