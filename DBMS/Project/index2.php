da815fa6a70ef2863d804a6e398a6c447bdb6bb0
ABHILASH <35189926+abhilashiswithu@users.noreply.github.com>
2018-11-29 11:09:42 +0530

    Add files via upload

---
+++ DBMS/Project/index2.php
@@ -0,0 +1,110 @@
+
+<?php
+	session_start();
+	
+	include("header.php");
+?>
+           <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line"><center><a href="index.php">MOVIE TICKET BOOKING</a></center></h1>
+               
+                         <div class="page-head-line">
+							 
+							 
+		<?php $_SESSION['uid']=1;
+		if($_SESSION['uid']!=NULL)
+       {
+		?>
+		  <form method="POST">
+					<input style="float: right; size="5px" type="submit" name="logout" class="btn btn-danger " value="Logout">
+				</form>   
+          <?php
+				if(isset($_POST['logout']))
+				{
+					date_default_timezone_set('Asia/Kolkata');
+
+					$timestamp = time();
+					$date_time = date("d-m-Y (D) H:i:s", $timestamp);
+					{
+						unset( $_SESSION['uid']);
+						header("location:index.php");
+					}
+				} 
+		}?>
+							 
+							 
+							 
+		
+					<br>
+                    </div>
+                        </div>
+                       
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                       <div class="panel panel-info">
+                       
+  
+                    <div class="table-responsive">
+                    	<div id="screen">
+                            <div class="alert alert-info">
+                           		 <center><strong><font color=#366B9A size=5px>MOVIES</font></strong></center>
+                            </div>
+					<form method="post">
+                            <table class="table  table-bordered table-hover1">
+                            
+                                    <tr>
+										<th><h3>Name</h3></th>
+										<th><h3>Genre</h3></th>
+										<th><h3>Duration</h3></th>
+										<th><h3>N0. of Show</h3></th>
+										<th><h3>Book</h3></th>
+                                    </tr>
+										
+										<?php 
+										
+											require("db.php");
+
+											$sql="select * from movie";
+											if($res = pg_query($db,$sql))
+											{
+												while($result=pg_fetch_row($res)){ ?>	
+                           <tr>
+								<td><?php echo"".$result[1]; ?></td>
+								<td><?php echo"".$result[4]; ?></td>
+								<td><?php echo"".$result[3]; ?></td>
+								<td><?php echo"".$result[2]; ?></td>
+								<td><input type="submit" size="10px" class="btn btn-xs btn-primary " name="submit" value="<?php echo"".$result[0]; ?>"></td>
+                           </tr>
+                           <?php }
+											}
+										?>
+								</table>
+                            </form>
+                          </div>
+                         </div>
+                  </div>        
+                <?php
+                    if(isset($_POST['submit']))
+                    {
+						$_SESSION['mid']=$_POST['submit'];
+						echo $_SESSION['mid'];
+						header("Location:index2.php");
+					}
+                    ?>
+                           </div>
+                        </div>
+                    
+                </div>
+
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    
+</body>
+</html>
---
+++ DBMS/Project/insert.sql
@@ -0,0 +1,31 @@
+﻿CREATE OR REPLACE FUNCTION c_insert(id INT,name VARCHAR(50),mail VARCHAR(100),pass VARCHAR(50),ph VARCHAR(20))
+RETURNS void
+AS
+$$
+BEGIN
+	insert into customer values (id+2,name,mail,pass,ph);
+END;
+$$
+LANGUAGE plpgsql;
+
+select c_insert(1,'Ram','ram@gmail.com','ram','9468233115');
+
+DROP FUNCTION c_insert(INT,VARCHAR(50),VARCHAR(100),VARCHAR(50),VARCHAR(20));
+
+create or replace function c_in_tri()
+returns trigger as
+$$
+begin
+	insert into customer_backup values (1,NEW.c_id,NEW.c_name,NEW.c_email,NEW.c_password,NEW.c_ph,TG_OP,NOW());
+	
+	insert into active_log values (1,NEW.c_id,NOW(),'');
+		return new;
+end;
+$$
+language plpgsql;
+
+create trigger c_intri after insert or update on customer
+for each row execute PROCEDURE c_in_tri();
+
+DROP TRIGGER c_intri on customer;
+DROP FUNCTION c_in_tri();
